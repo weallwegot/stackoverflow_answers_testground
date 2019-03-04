35cf6bfba56cedab48be7e417a3b0c5316cfde33
tosin <tosin.sonuyi@gmail.com>
2019-03-03 19:00:36 -0500

    add retrieving type from the error

--- exception_raise_custom.py
+++ exception_raise_custom.py
@@ -1,8 +1,7 @@
 # someone wants to catch any type of exception but then also raise the message
 # i dont quite understand the motivation
 try:
-
     # some code that raises an error, like type error
     g = 10 + '11'
 except Exception as e:
-    raise AssertionError(str(e))
+    raise AssertionError('{}: {}'.format(type(e), e.message))
